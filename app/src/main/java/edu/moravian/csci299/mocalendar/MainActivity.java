0413d4ed01df4fc3466891f7dd2e6ed47831daf6
evan-toyberg <78161344+evan-toyberg@users.noreply.github.com>
2021-04-11 19:22:57 -0400

    Make calendar fragment show up

--- app/src/main/java/edu/moravian/csci299/mocalendar/MainActivity.java
+++ app/src/main/java/edu/moravian/csci299/mocalendar/MainActivity.java
@@ -1,8 +1,12 @@
 package edu.moravian.csci299.mocalendar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import java.util.Date;
 
 /**
  * The main (and only) activity for the application that hosts all of the fragments.
@@ -18,12 +22,30 @@ import android.os.Bundle;
  * NOTE: This Activity is the bare-bones, empty, Activity. Work will be definitely needed in
  * onCreate() along with implementing some callbacks.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CalendarFragment.Callbacks {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();  // get fragment manager
+        FragmentTransaction transaction = fm.beginTransaction();  // begin transaction with fragment manager
+        CalendarFragment calendarFragment = (CalendarFragment) fm.findFragmentById(R.id.calendarView);  // get calendar fragment
+        if(calendarFragment == null) { calendarFragment = CalendarFragment.newInstance(); }  // create new instance of CalendarFragment if it cannot be found
+        ListFragment listFragment = ListFragment.newInstance();  // get new list fragment
+        transaction.add(R.id.fragment_container, calendarFragment).add(R.id.fragment_container, listFragment).commit(); // add both fragments to main view
+
+        // todo : still need to figure out how to get clicks on dates to register and show events for current date
+        // Also make sure the fragments show up in correct positions
+
+
+
+
     }
 
 
+    @Override
+    public void onDayChanged(Date date) {
+
+    }
 }
